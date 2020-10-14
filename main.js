8df2c54136993d02b64107f5a97af34bc1bc7440
redstewart <reedostewart@gmail.com>
2020-10-15 11:34:00 +1300

    added minor validation

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 document.getElementById("formatBtn").addEventListener("click", () => {
   const textArea = document.getElementById("textArea");
-  let proxyArr = textArea.value.split("\n");
-  let formattedArr = proxyArr.map((el) => {
-    let splitArr = el.split(":");
-    return `${splitArr[2]}:${splitArr[3]}@${splitArr[0]}:${splitArr[1]}`;
-  });
-  formattedArr = formattedArr.join("\n");
-  textArea.value = formattedArr;
+  if (textArea.value) {
+    let proxyArr = textArea.value.split("\n");
+    let formattedArr = proxyArr.map((el) => {
+      let splitArr = el.split(":");
+      return `${splitArr[2]}:${splitArr[3]}@${splitArr[0]}:${splitArr[1]}`;
+    });
+    formattedArr = formattedArr.join("\n");
+    textArea.value = formattedArr;
+  }
 });
 
 document.getElementById("copyBtn").addEventListener("click", () => {
